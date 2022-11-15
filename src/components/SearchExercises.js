0a240155acecdf63433d4a5131d78a3a6230dbf5
yash-agrwl <agarwalyash7012@gmail.com>
2022-11-15 16:22:43 +0530

    implemented HorizontalScrollbar and used rapid Api to fetch bodyPartList on page load

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,10 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     const [search, setSearch] = useState('');
-    const [exercises, setExercises] = useState([]);
+    const [bodyParts, setBodyParts] = useState([]);
+
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+            setBodyParts(['all', ...bodyPartsData]);
+        }
+
+        fetchExercisesData();
+    }, [])
+
 
     const handleSearch = async () => {
         if (search) {
@@ -37,12 +49,11 @@ const SearchExercises = () => {
                     sx={{
                         input: {
                             fontWeight: '700',
-                            border: 'none',
-                            borderRadius: '4px'
+                            border: 'none'
                         },
                         width: { lg: '1170px', xs: '350px' },
                         backgroundColor: '#fff',
-                        borderRadius: '40px'
+                        borderRadius: '4px',
                     }}
                     height='76px' value={search}
                     onChange={(e) => setSearch(e.target.value.toLowerCase())}
@@ -63,6 +74,13 @@ const SearchExercises = () => {
                 >
                     Search
                 </Button>
+
+            </Box>
+
+            <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+
+                <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart}
+                    setBodyPart={setBodyPart} />
 
             </Box>
 
